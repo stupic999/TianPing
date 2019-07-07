0e960c0a9ff6b8a44e32f41886bb6a852772f8c1
stupid999 <aierzhiguang2@gmail.com>
2019-07-07 19:43:10 +0800

    完成天平初步效果

---
+++ Assets/Plate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plate : MonoBehaviour {
+
+    public Transform plate;
+    float plateX;
+
+    private void Start()
+    {
+        plateX = transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        transform.position = new Vector3(plateX, plate.position.y, 0);
+        transform.rotation = new Quaternion(0, 0, 0, 0);
+	}
+}
